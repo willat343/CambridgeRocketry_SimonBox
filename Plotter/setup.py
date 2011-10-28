a9e8fbe710d43857571a1bc69eac40b6ed7264e2
Simon <simon@StudioLaptop.(none)>
2011-10-28 11:40:37 +0100

    licencing info updated

--- Plotter/setup.py
+++ Plotter/setup.py
@@ -1,3 +1,24 @@
+## Copyright (C) 2010 S.Box
+## 
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+## setup.py
+
+## Author: S.Box
+## Created: 2011-10-27
+
 __author__="simon"
 __date__ ="$29-Jul-2011 13:02:42$"
 
